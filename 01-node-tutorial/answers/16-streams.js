65fd1a55ab9923f7e1279c44f5e280cf83c17aa0
Alina Dalantaeva <elina.d32@yandex.ru>
2024-11-22 19:24:13 -0500

    Updated 16-streams.js

--- 01-node-tutorial/answers/16-streams.js
+++ 01-node-tutorial/answers/16-streams.js
@@ -2,8 +2,14 @@ const { createReadStream } = require("fs");
 
 const stream = createReadStream("../content/big.txt", { encoding: "utf-8", highWaterMark: 200});
 
-stream.on("data", (result) => {
-    console.log(result);
+let chunkCount = 0;
+
+stream.on("data", (chunk) => {
+    console.log(chunk);
+    chunkCount++;
 });
 
+stream.on("end", () => {
+    console.log(chunkCount);
+});
 stream.on("error", (err) => {console.log(err)});
