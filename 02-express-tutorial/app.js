cc583470454f3bdaba95bde4553c97ad52bcdee7
Alina Dalantaeva <elina.d32@yandex.ru>
2024-12-18 00:42:39 -0500

    middleware, http methods, testing with postman, refactoring

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,60 @@
 const { products } = require("./data");
 const express = require("express");
 const app = express();
+const peopleRouter = require('./routes/people');
+
+// app.use(express.static('./methods-public'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api/v1/people', peopleRouter);
+
+// app.get('/api/v1/people', (req, res) => {
+//     res.status(200).json({ success: true, data: people });
+// });
+
+// app.post('/api/v1/people', (req, res) => {
+//     const { name } = req.body;
+//     if(!name){
+//         return res.status(400).json({ success: false, message: 'Please provide a name' })
+//     }
+//     people.push({ id: people.length + 1, name: req.body.name });
+//     res.status(201).json({ success: true, name: req.body.name })
+// });
+
+// app.put('/api/v1/people/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { name } = req.body;
+
+//     const person = people.find((person) => person.id === Number(id))
+//     if(!person){
+//         return res.status(400).json({ success: false, message: `No person with id ${id}` })
+//     }
+//     const newPeople = people.map((person) => {
+//         if(person.id === Number(id)){
+//             person.name = name
+//         }
+//         return person
+//     });
+//     res.status(200).json({success: true, data: newPeople })
+// });
+
+// app.delete('/api/v1/people/:id', (req, res) => {
+//     const person = people.find((person) => person.id === Number(req.params.id))
+//     if(!person){
+//         return res.status(400).json({ success: false, message: `No person with id ${req.params.id}` })
+//     }
+//     const newPeople = people.filter((person) => person.id !== Number(req.params.id));
+
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().toISOString();
+    console.log(method, url, time);
+    next();
+};
+
+app.use(logger);
 
 app.use(express.static("./public"));
 
